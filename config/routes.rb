6a7a97da034fc8b22ef8c82126b40166c2b883c7
Nick ODonoghue <nicholasgodonoghue@gmail.com>
2023-02-21 16:37:19 +0000

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # READ (ALL)
+  get 'tasks', to: 'tasks#index'
+  # CREATE (GET)
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  # READ (ONE)
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # UPDATE (GET)
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  patch 'tasks/:id', to: 'tasks#update'
+  # DELETE
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete
 end
